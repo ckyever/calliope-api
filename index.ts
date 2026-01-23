bc63d75194abb30353fd6e982abbc6312783e58c
Ckyever Gaviola <ckyeverg@gmail.com>
2026-01-23 15:10:54 +1100

    Setup passport and session

--- index.ts
+++ index.ts
@@ -1,10 +1,28 @@
+import "dotenv/config";
 import express from "express";
 import type { Request, Response } from "express";
+import passport from "passport";
+import session from "express-session";
 
 const app = express();
 
+const SESSION_SECRET = process.env.SESSION_SECRET;
+if (!SESSION_SECRET) {
+  throw Error("Missing session secret environment variable");
+}
+
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (request: Request, response: Response) =>
-  response.send("Hello, world!"),
+  response.send("Welcome to the Calliope API!"),
 );
 
 const PORT = process.env.PORT || 3000;
@@ -12,5 +30,5 @@ app.listen(PORT, (error) => {
   if (error) {
     throw error;
   }
-  console.log(`App started on ${new Date().toISOString()}`);
+  console.log(`App started on ${new Date()}`);
 });
